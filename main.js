69e877b6a90236607968e4661be26673645a2458
Antoine Drouault <antoinedrouault@hotmail.com>
2023-06-30 23:36:57 +0200

    Make category scale elements length dependant of bmi categories

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const heightInput = document.getElementById("height");
 const weightInput = document.getElementById("weight");
 const submitButton = document.getElementById("submit");
 const bmiElement = document.getElementById("bmi");
-const descriptionElement = document.getElementById("description");
 const scaleElement = document.getElementById("scale");
 const categoriesElements = document.getElementsByClassName("categories");
+const bmiTextElement = document.getElementById("bmi-text");
 
 const categories = [
   { name: "Underweight (Severe thinness) ", min: 0, max: 16 },
@@ -16,6 +16,8 @@ const categories = [
   { name: "Obese (Class II) ", min: 35, max: 40 },
   { name: "Obese (Class III) ", min: 40, max: 1 * Math.pow(10, 99) },
 ];
+
+const maxBMI = 50;
 
 submitButton.addEventListener("click", handleSubmit);
 
@@ -29,18 +31,24 @@ function getBMI() {
   const weight = weightInput.value;
   const bmi = Math.round((weight / Math.pow(height / 100, 2)) * 100) / 100;
   bmiElement.textContent = bmi;
-  descriptionElement.textContent = categories.find(
-    (x) => bmi >= x.min && bmi < x.max
-  ).name;
 }
 
 function createScale() {
   if (!scaleElement.classList.contains("show")) {
     scaleElement.classList.add("show");
+    categories.forEach((category, index, array) => {
+      let span = document.createElement("span");
+
+      span.textContent = category.name;
+      categoriesElements[array.length - 1 - index].append(span);
+      if (category.max > maxBMI) {
+        categoriesElements[array.length - 1 - index].style.height =
+          ((maxBMI - category.min) / maxBMI) * 100 + "%";
+      } else {
+        categoriesElements[array.length - 1 - index].style.height =
+          ((category.max - category.min) / maxBMI) * 100 + "%";
+      }
+    });
+    bmiTextElement.style.top = "50%";
   }
-  categories.forEach((category, index) => {
-    let span = document.createElement("span");
-    span.textContent = category.name;
-    categoriesElements[index].append(span);
-  });
 }
